321a5a6e07977a3312a4bde23ad92fe2cf37d7c1
Mateus Silva <thi.hero2012@gmail.com>
2026-01-16 13:35:58 -0300

    fix: api not working

--- api_server.py
+++ api_server.py
@@ -317,5 +317,5 @@ if __name__ == '__main__':
     print("=" * 50)
     
     ensure_csv_exists()
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
 
